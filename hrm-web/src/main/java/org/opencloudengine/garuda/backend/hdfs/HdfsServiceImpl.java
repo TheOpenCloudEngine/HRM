ae6a015ebe81e9edc887b4e037cc4f843b338a82
SeungpilPark <sppark@uengine.org>
2016-09-01 13:16:50 +0900

    file upload

--- hrm-web/src/main/java/org/opencloudengine/garuda/backend/hdfs/HdfsServiceImpl.java
+++ hrm-web/src/main/java/org/opencloudengine/garuda/backend/hdfs/HdfsServiceImpl.java
@@ -196,22 +196,37 @@ public class HdfsServiceImpl implements HdfsService {
         FSDataOutputStream out = fs.append(fsPath);
         byte[] b = new byte[1024];
         long status = 0;
-        int numBytes = 0;
+        //int numBytes = 0;
         int totalread = 0;
         int count = 0;
-        while ((numBytes = is.read(b)) > 0) {
+
+        for (int numBytes = is.read(b); numBytes >= 0; numBytes = is.read(b)) {
+            out.write(b, 0, numBytes);
             count++;
             totalread += numBytes;
             status = ((totalread * 100) / size);
             if (count % 1000 == 0) {
+                System.out.println(size);
                 System.out.println(totalread);
                 System.out.println(totalread / size);
                 System.out.println(((totalread * 100) / size));
                 System.out.println(status);
                 this.setProgress(uuid, (int) status);
             }
-            out.write(b, 0, numBytes);
         }
+//        while ((numBytes = is.read(b)) > 0) {
+//            count++;
+//            totalread += numBytes;
+//            status = ((totalread * 100) / size);
+//            if (count % 1000 == 0) {
+//                System.out.println(totalread);
+//                System.out.println(totalread / size);
+//                System.out.println(((totalread * 100) / size));
+//                System.out.println(status);
+//                this.setProgress(uuid, (int) status);
+//            }
+//            out.write(b, 0, numBytes);
+//        }
         this.setProgress(uuid, 100);
 
         is.close();
